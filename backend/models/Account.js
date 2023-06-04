269704996215b8899ef7c18d994778d03c9bbf73
joao vitor <githubconta2021@gmail.com>
2023-06-04 12:02:08 -0400

    Descriçao: finalizando os models, agora vamos para os controllers

--- backend/models/Account.js
+++ backend/models/Account.js
@@ -4,5 +4,15 @@ const { Schema } = mongoose;
 const AccountSchema = new Schema({
      Accountname: String,
      username: String,
-     
-})
+     accountBalance: Number,
+     userId: Object.userId,
+     userName: String,
+},
+{
+    timestamps: true,
+}
+);
+
+Account = mongoose.model("account", AccountSchema); 
+
+module.exports = Account
